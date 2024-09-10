1e68c4e5b76ef33019d6362bb2698c1f2628a4e6
McVyp <lyeclyde@gmail.com>
2024-09-10 10:27:24 +0900

    render a html page

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer =[0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let response  = "HTTP/1.1. 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    let response  = format!(
+        "HTTP/1.1. 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
